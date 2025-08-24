895c2b161bac62ef9a0eab9124bbfddd58e141fa
jedson Ferreira <jedazevedo@gmail.com>
2025-08-24 02:23:50 -0300

    Implementação dos inimigos 1 e inimigos 2, player1 e player2

--- MountainShooter/code/enemy.py
+++ MountainShooter/code/enemy.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-from Entity import Entity
-
+import pygame
+from MountainShooter.code.entity import Entity
+from MountainShooter.code.const import ENTITY_SPEED, WIN_WIDTH
 
 class Enemy(Entity):
-    def __init__(self):
-        pass
+    def __init__(self, name, position):
+        super().__init__(name, position)
 
     def move(self, ):
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rect.right<=0:
+            self.rect.left = WIN_WIDTH
         pass
--- MountainShooter/code/level.py
+++ MountainShooter/code/level.py
@@ -4,9 +4,10 @@ import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 import sys
+import random
 from MountainShooter.code.entity import Entity
 from MountainShooter.code.entityFactory import EntityFactory
-from MountainShooter.code.const import C_WHITE, WIN_HEIGHT
+from MountainShooter.code.const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 
 class Level:
     def __init__(self, window, name, game_mode):
@@ -15,7 +16,13 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity(self.name + 'Bg'))
+        self.entity_list.append(EntityFactory.get_entity('Player1'))
         self.timeout = 20000
+        if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity('Player2'))
+
+        pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
+
 
     def run(self):
         pygame.mixer_music.load(f'./asset/Level1.mp3')
@@ -30,6 +37,9 @@ class Level:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == EVENT_ENEMY:
+                    choise = random.choice(('Enemy1','Enemy2'))
+                    self.entity_list.append(EntityFactory.get_entity(choise))
 
             # printed text
             self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', C_WHITE, (10, 5))
