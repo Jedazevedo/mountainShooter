b482256f90ad74161dcdb4ec127cc000ed6c692c
jedson Ferreira <jedazevedo@gmail.com>
2025-08-24 17:20:42 -0300

    implementação de logica de destruir entity Enemy e criacao de nova entidade entitymediator

---
+++ MountainShooter/code/entityMediator.py
@@ -0,0 +1,21 @@
+from MountainShooter.code.entity import Entity
+from MountainShooter.code.enemy import Enemy
+class EntityMediator:
+
+    @staticmethod
+    def __verify_collision_window(ent: Entity):
+        if isinstance(ent, Enemy):
+            if ent.rect.right <0:
+                ent.health = 0
+
+    @staticmethod
+    def verify_collision(entity_list: list[Entity]):
+        for i in range(len(entity_list)):
+            entity_teste = entity_list[i]
+            EntityMediator.__verify_collision_window(entity_teste)
+
+    @staticmethod
+    def verify_health(entity_list: list[Entity]):
+        for ent in entity_list:
+            if ent.health <= 0:
+                entity_list.remove(ent)
--- MountainShooter/code/level.py
+++ MountainShooter/code/level.py
@@ -8,6 +8,7 @@ import random
 from MountainShooter.code.entity import Entity
 from MountainShooter.code.entityFactory import EntityFactory
 from MountainShooter.code.const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
+from MountainShooter.code.entityMediator import EntityMediator
 
 class Level:
     def __init__(self, window, name, game_mode):
@@ -46,9 +47,10 @@ class Level:
             self.level_text(14, f'fps: {clock.get_fps():.0f}', C_WHITE, (10, WIN_HEIGHT - 35))
             self.level_text(14, f'entidades: {len(self.entity_list)}', C_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
+
             # Collisions
-            # EntityMediator.verify_collision(entity_list=self.entity_list)
-            # EntityMediator.verify_health(entity_list=self.entity_list)
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
